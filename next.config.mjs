831a5e6bfd0c620d5bf0f7b3405d2fadb5750b04
restrainhalation <168318744+restrainhalation@users.noreply.github.com>
2024-04-29 16:53:11 +0900

    🛠️ chore: use static export #2

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,17 @@ const withBundleAnalyzer = bundleAnalyzer({
 });
 
 export default withBundleAnalyzer({
+  output: 'export',
+
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+
   reactStrictMode: false,
   eslint: {
     ignoreDuringBuilds: true,
