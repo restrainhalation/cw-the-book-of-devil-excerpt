a4f9e4a3cdc8e20e84d86aaf1a4841f5951cbcae
restrainhalation <168318744+restrainhalation@users.noreply.github.com>
2024-05-05 21:59:44 +0900

    👷 feat: use skeletons to create a rough layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,23 @@
-import { Welcome } from '@/components/Welcome/Welcome';
-import { ColorSchemeToggle } from '@/components/ColorSchemeToggle/ColorSchemeToggle';
+import { Title, Grid, GridCol, Skeleton, Container } from '@mantine/core';
 
-export default function HomePage() {
+export default function Index() {
   return (
     <>
-      <Welcome />
-      <ColorSchemeToggle />
+      <Title ta="center" mt={50}>
+        タイトル
+      </Title>
+      <Container my="md">
+        <Grid>
+          <GridCol span={{ base: 12, xs: 6 }}>
+            <Skeleton height={70} radius="md" animate={false} my={20} />
+            <Skeleton height={90} radius="md" animate={false} my={20} />
+            <Skeleton height={140} radius="md" animate={false} my={20} />
+          </GridCol>
+          <GridCol span={{ base: 12, xs: 6 }}>
+            <Skeleton height={340} radius="md" animate={false} my={20} />
+          </GridCol>
+        </Grid>
+      </Container>
     </>
   );
 }
