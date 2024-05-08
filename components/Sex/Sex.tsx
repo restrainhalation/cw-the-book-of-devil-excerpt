ed7eb033cc61d7725cd93d5a35a33062fb212d2d
restrainhalation <168318744+restrainhalation@users.noreply.github.com>
2024-05-09 08:14:53 +0900

    🧩 feat: create component for sex input

---
+++ components/Sex/Sex.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import React, { FC, useState } from 'react';
+import { Group, Chip } from '@mantine/core';
+import { SEXES } from '@/constants';
+
+/**
+ * 性別コンポーネントのパラメータ
+ * @typedef SexParams
+ * @property {((sexId:number) => void) | undefined} onChange 値を変更した際に実行するメソッド
+ */
+
+/**
+ * 性別コンポーネント
+ * @param {SexParams} param0 コンポーネントのパラメータ
+ * @return {React.FC<SexParams>} コンポーネント
+ */
+const SexInput: FC<{ onChange?: (sexId:number) => void; }> = ({ onChange }) => {
+  // ON になっている性別の ID
+  const [selectedId, setSelectedId] = useState<number>();
+
+  /**
+   * 当該性別の ON／OFF が変更されたときに実行する
+   * 値を変更した際に実行するメソッドがあれば実行する
+   * @param {number} sexId 変更があった性別の ID
+   */
+  const handleChangeSex = (sexId:number) => {
+    if (selectedId === sexId) {
+      // ON to OFF
+      // ---
+      // ON になっている性別を undefined にする
+      setSelectedId(undefined);
+    } else {
+      // ON to OFF
+      // ---
+      // ON になっている性別を変更があった性別の ID にする
+      setSelectedId(sexId);
+    }
+
+    // 値を変更した際に実行するメソッドがあれば実行する
+    if (onChange) onChange(sexId);
+  };
+
+  return (
+    <>
+      <Group justify="center">
+        {Object.values(SEXES).map((sex) => (
+            <Chip
+              key={sex.id}
+              value={sex.id}
+              variant="outline"
+              checked={selectedId === sex.id}
+              onClick={() => handleChangeSex(sex.id)}
+            >
+              {sex.name}
+            </Chip>
+          ))}
+      </Group>
+    </>
+  );
+};
+
+export default SexInput;
