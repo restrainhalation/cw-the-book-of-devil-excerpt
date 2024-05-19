967de06b05c7606b4c94bc7e713b35f8cc3b57a5
restrainhalation <168318744+restrainhalation@users.noreply.github.com>
2024-05-19 22:45:20 +0900

    🏷 feat: create component of ability infomation list

---
+++ components/AbilityInfomationList/AbilityInfomationList.tsx
@@ -0,0 +1,153 @@
+import React, { FC, ForwardRefExoticComponent, RefAttributes } from 'react';
+import { Badge, Text, Group, rem } from '@mantine/core';
+import {
+  Icon, IconProps,
+  IconArrowUpRight, IconArrowDownRight,
+} from '@tabler/icons-react';
+import type { Physical, Mental } from '@/types';
+import { PHYSICAL_ABILITIES, MENTAL_ABILITIES } from '@/constants';
+
+/** 能力の ID と出力順のマップ */
+const ABILITY_ORDER_MAP: { [id: string]: number } = Object.assign(
+  {},
+  ...PHYSICAL_ABILITIES.map((physicalAbility) => ({ [physicalAbility.id]: physicalAbility.order })),
+  ...MENTAL_ABILITIES.map((mentalAbility) => ({ [mentalAbility.id]: mentalAbility.order }))
+);
+
+/** 能力の ID とアイコンコンポーネントのマップ */
+const ABILITY_ICON_MAP: {
+  [id: string]: ForwardRefExoticComponent<Omit<IconProps, 'ref'> & RefAttributes<Icon>>
+} = Object.assign(
+  {},
+  ...PHYSICAL_ABILITIES.map((physicalAbility) => ({ [physicalAbility.id]: physicalAbility.icon })),
+  ...MENTAL_ABILITIES.map((mentalAbility) => ({ [mentalAbility.id]: mentalAbility.icon }))
+);
+
+/**
+ * 能力名タグコンポーネントのパラメータ
+ * @typedef AbilityNameTagParams
+ * @property {string} id 能力 ID
+ * @property {boolean} isPhysical 身体的特徴であるか
+ */
+
+/**
+ * 能力名タグコンポーネント
+ * @param {AbilityNameTagParams} param0 コンポーネントのパラメータ
+ * @return {React.FC<AbilityNameTagParams>} コンポーネント
+ */
+const AbilityNameTag: FC<{
+  id: string,
+  isPhysical: boolean,
+}> = ({ id, isPhysical }) => {
+  const AbilityNameTagIcon = ABILITY_ICON_MAP[id];
+  return (
+    <Badge
+      size="sm"
+      color={isPhysical ? 'violet.5' : 'violet.9'}
+      leftSection={
+        <AbilityNameTagIcon style={{ width: rem(12), height: rem(12) }} />
+      }
+    >
+      {
+        isPhysical
+          ? (PHYSICAL_ABILITIES.find((physicalAbility) => physicalAbility.id === id)?.name || '')
+          : (MENTAL_ABILITIES.find((mentalAbility) => mentalAbility.id === id)?.nameOfBoth1 || '')
+      }
+    </Badge>
+  );
+};
+
+/**
+ * 能力情報コンポーネントのパラメータ
+ * @typedef AbilityInfomationParams
+ * @property {string} id 能力 ID
+ * @property {number} value 能力の値
+ * @property {boolean | undefined} isPhysical 身体的特徴であるか
+ * @property {boolean | undefined} isPositive 精神的特徴におけるポジティブ値であるか
+ */
+
+/**
+ * 能力情報コンポーネント
+ * @param {AbilityInfomationParams} param0 コンポーネントのパラメータ
+ * @return {React.FC<AbilityInfomationParams>} コンポーネント
+ */
+const AbilityInfomation: FC<{
+  id: string,
+  value: number,
+  isPhysical?: boolean,
+  isPositive?: boolean,
+}> = ({ id, value, isPhysical, isPositive }) => {
+  const DiffIcon = isPositive ? IconArrowUpRight : IconArrowDownRight;
+  const valueTextColorClass = isPositive ? 'text-teal-700' : 'text-red-800';
+  const valueClasses = `${valueTextColorClass} leading-none flex items-center`;
+  return (
+    <Group justify="space-between" m="2" className="w-40">
+      <AbilityNameTag id={id} isPhysical={!!isPhysical} />
+      <Text className={valueClasses}>
+        <span>{value}</span>
+        <DiffIcon size="1rem" stroke={1.5} className="flex" />
+      </Text>
+    </Group>
+  );
+};
+
+/**
+ * 能力情報一覧コンポーネントのパラメータ
+ * @typedef AbilityInfomationListParams
+ * @property {Physical} physical 身体的特徴
+ * @property {Mental} mental 精神的特徴
+ */
+
+/**
+ * 能力情報一覧コンポーネント
+ * @param {AbilityInfomationListParams} param0 コンポーネントのパラメータ
+ * @return {React.FC<AbilityInfomationListParams>} コンポーネント
+ */
+export const AbilityInfomationList: FC<{
+  physical: Physical,
+  mental: Mental
+}> = ({ physical, mental }) => {
+  const physicalValues = Object.entries(physical)
+    .filter(([, value]) => value !== 0)
+    .map(([key, value]) => ({
+      id: key,
+      value,
+      isPhysical: true,
+      isPositive: value > 0,
+    }));
+  const mentalValues = Object.entries(mental)
+    .filter(([, value]) => value !== 0)
+    .map(([key, value]) => ({
+      id: key,
+      value,
+      isPhysical: false,
+      isPositive: value > 0,
+    }));
+  return (
+    <>
+      {physicalValues
+        .concat(mentalValues)
+        .sort((a, b) =>
+          a.isPhysical !== b.isPhysical
+            ? (
+                a.isPhysical
+                  ? -1
+                  : 1
+              )
+            : a.isPhysical === b.isPhysical &&
+                (ABILITY_ORDER_MAP[a.id] || 0) < (ABILITY_ORDER_MAP[b.id] || 0)
+              ? -1
+              : 1
+        )
+        .map((ability) =>
+          <AbilityInfomation
+            key={ability.id}
+            id={ability.id}
+            value={ability.value}
+            isPhysical={ability.isPhysical}
+            isPositive={ability.isPositive}
+          />
+      )}
+    </>
+  );
+};
