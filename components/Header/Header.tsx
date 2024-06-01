e32c2ee49ad9c48b7b7d19334d1933ec96c89fa6
restrainhalation <168318744+restrainhalation@users.noreply.github.com>
2024-06-01 22:31:38 +0900

    🔘 feat: update design of reset button

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,9 +1,12 @@
 'use client';
 
-import { Button, Container, Group, Title } from '@mantine/core';
+import { Container, Group, Title, Tooltip, TooltipGroup, UnstyledButton } from '@mantine/core';
 import { useSetAtom } from 'jotai';
+import { IconReload } from '@tabler/icons-react';
+import cx from 'clsx';
 import { abilityAtom, abilityReferenceAtom, characterAtom, keyForResetAtom } from '@/store';
 import { DEFAULT_ABILITY_ATOM, DEFAULT_CHARACTER_ATOM } from '@/constants';
+import classes from './Header.module.css'
 
 export function Header() {
   // Jotai のキャラクター atom
@@ -35,9 +38,15 @@ export function Header() {
         <Title className="inline-block">
           タイトル
         </Title>
-        <Group justify="center" className="inline-flex">
-          <Button onClick={reset}>RESET</Button>
-        </Group>
+        <TooltipGroup openDelay={600} closeDelay={100}>
+          <Group justify="center" className="inline-flex" gap="xs">
+            <Tooltip label="リセット">
+              <UnstyledButton onClick={reset} className={cx(classes.control, classes.button)} component="button">
+                <IconReload className={classes.icon} />
+              </UnstyledButton>
+            </Tooltip>
+          </Group>
+        </TooltipGroup>
       </Container>
     </>
   );
