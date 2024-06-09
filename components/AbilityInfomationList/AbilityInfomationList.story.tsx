8ef3eba8ff6c0424c38a1791fcd0eae3d728ffc2
restrainhalation <168318744+restrainhalation@users.noreply.github.com>
2024-06-09 14:16:29 +0900

    ✂ separate story of ability infomation list from story of ability tooltip

--- components/AbilityInfomationList/AbilityInfomationList.story.tsx
+++ components/AbilityInfomationList/AbilityInfomationList.story.tsx
@@ -1,8 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { Chip, Group } from '@mantine/core';
 import { AbilityInfomationList } from './AbilityInfomationList';
-import { NATURES, PERIODS } from '@/constants';
-import { AbilityTooltip } from '@/components/AbilityTooltip';
+import { NATURES } from '@/constants';
+
+const sampleNature = NATURES[2]
 
 const meta: Meta<typeof AbilityInfomationList> = {
   title: 'AbilityInfomationList',
@@ -15,32 +15,8 @@ export default meta;
 type Story = StoryObj<typeof AbilityInfomationList>;
 
 export const Default: Story = {
-  render: () => (
-    <AbilityInfomationList
-      physical={NATURES[2].physical}
-      mental={NATURES[2].mental}
-    />
-  ),
-};
-
-export const UseOnTooltip: Story = {
-  render: () => (
-    <>
-      <Group justify="center">
-        {PERIODS.map((period) => (
-          <AbilityTooltip key={period.id} physical={period.physical} mental={period.mental}>
-            <div>
-              <Chip
-                key={period.id}
-                value={period.id}
-                variant="outline"
-              >
-                {period.name}
-              </Chip>
-            </div>
-          </AbilityTooltip>
-        ))}
-      </Group>
-    </>
-  ),
+  args: {
+    physical: sampleNature.physical,
+    mental: sampleNature.mental,
+  },
 };
