de7092f3c8eb86d361d04901ab1549cfc05b369d
restrainhalation <168318744+restrainhalation@users.noreply.github.com>
2024-06-15 14:21:37 +0900

    🪵 chore: fix base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 import bundleAnalyzer from '@next/bundle-analyzer';
 
 // 環境差を埋めるためのパスのプレフィックス
-const prefix = process.env.NODE_ENV === 'production' ? '/training-github-workflow' : ''
+const prefix = process.env.NODE_ENV === 'production' ? '/cw-the-book-of-devil-excerpt' : ''
 
 const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
