cc7a3dc75dc683ac0668b520472791d83e6b7ba5
restrainhalation <168318744+restrainhalation@users.noreply.github.com>
2024-06-15 14:16:31 +0900

    🪚 chore: remove step of actions/configure-pages@v5 #2

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,17 @@
 import bundleAnalyzer from '@next/bundle-analyzer';
 
+// 環境差を埋めるためのパスのプレフィックス
+const prefix = process.env.NODE_ENV === 'production' ? '/training-github-workflow' : ''
+
 const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
 });
 
 export default withBundleAnalyzer({
+  images: { unoptimized: true },
+  assetPrefix: prefix,
+  basePath: prefix,
   output: 'export',
-
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
-
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
-
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
 
   reactStrictMode: false,
   eslint: {
